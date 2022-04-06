8051d5a96a0b78a8584170d140f6295aed35f2e2
Pedro Henrique <46581272+PedroAzara@users.noreply.github.com>
2022-04-06 17:07:32 -0300

    Create Home.vue

---
+++ Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p >You are searching for: {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">{{name}}</div>
+  </div>
+</template>
+
+<script>
+
+import {ref, computed} from 'vue'
+export default {
+  name: 'Home',
+  setup(){
+    const search = ref('')
+    const names = ref(['pedro', 'mario', 'shaun', 'maran', 'sauan', 'saron'])
+
+  
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+
+    })
+
+      return {names, search, matchingNames}
+  }
+}
+</script>
